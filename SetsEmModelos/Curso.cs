515e59ab02a047180fcb4ceeb003943034ca5982
eduBrbosa <d.dbarbosa142@gmail.com>
2025-10-26 16:35:20 -0300

    Usando sets dentro dos modelos do projeto

---
+++ SetsEmModelos/Curso.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetsEmModelos
+{
+    class Curso
+    {
+		private ISet<Aluno> alunos = new HashSet<Aluno>();
+        public IList<Aluno> Alunos
+		{ 
+			get
+			{
+				return new ReadOnlyCollection<Aluno>(alunos.ToList());
+			} 
+		}
+
+        private List<Aula> aulas;
+
+		public IList<Aula> Aulas
+		{
+			get { return new ReadOnlyCollection<Aula>(aulas); }
+		}
+
+		private string nome;
+		private string instrutor;
+
+
+        public string Nome
+		{
+			get { return nome; }
+			set { nome = value; }
+		}
+
+
+		public string Instrutor
+		{
+			get { return instrutor; }
+			set { instrutor = value; }
+		}
+
+        public Curso(string nome, string instrutor)
+        {
+            this.nome = nome;
+            this.instrutor = instrutor;
+			aulas = new List<Aula>();
+        }
+
+		public int TempoTotal
+		{
+			get
+			{
+				//int total = 0;
+				//foreach(var aula in aulas)
+				//{
+				//	total += aula.Tempo;
+				//}
+				//return total;
+
+				return aulas.Sum(aula => aula.Tempo);
+			}
+		}
+
+        internal void Adiciona(Aula aula)
+        {
+            aulas.Add(aula);
+        }
+
+        public override string ToString()
+        {
+			return $"Curso: {nome}, Tempo: {TempoTotal},\n Aulas: {string.Join(",\n", aulas)}";
+        }
+
+        internal void Matricula(Aluno aluno)
+        {
+			alunos.Add(aluno);
+        }
+    }
+}
---
+++ SetsEmModelos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetsEmModelos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso cSharpColecoes = new Curso("C# Colecoes", "Marcelo Oliveira");
+
+            cSharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
+            cSharpColecoes.Adiciona(new Aula("Criando uma Aula", 20));
+            cSharpColecoes.Adiciona(new Aula("Modelando com Colecoes", 24));
+
+            Aluno a1 = new Aluno("Vitória Correa", 34561);
+            Aluno a2 = new Aluno("Pierre dos Santos", 34935);
+            Aluno a3 = new Aluno("Eduardo Dias", 34672);
+
+            cSharpColecoes.Matricula(a1);
+            cSharpColecoes.Matricula(a2);
+            cSharpColecoes.Matricula(a3);
+
+
+            Console.WriteLine("Imprimindo os alunos matriculados: ");
+            foreach (var aluno in cSharpColecoes.Alunos)
+            {
+                Console.WriteLine(aluno.ToString());                
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
